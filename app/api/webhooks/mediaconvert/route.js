5c55a0f3425205fdfad6055a2e3538c8b855f4b8
shivam02544 <anteshkumar114@gmail.com>
2026-04-09 01:26:38 +0530

    feat: ReactPlayer HLS player + idempotent MediaConvert webhook

--- app/api/webhooks/mediaconvert/route.js
+++ app/api/webhooks/mediaconvert/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/db";
+import { connectDB } from "@/lib/mongodb";
 import Video from "@/models/Video";
 import { logger } from "@/lib/logger";
 import { getCdnUrl } from "@/lib/cdn";
